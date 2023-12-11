7ce3151d7f252306277029e65f2be07fd8538f43
4rcan31 <da.esfra12@gmail.com>
2023-12-11 17:25:18 -0600

    Fix bug in inner class HttpResponse.Builder

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -8,19 +8,26 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Serve server = new Serve(8080);
-
+            Serve server = new Serve(8081);
+    
             server.addRoute(HttpMethods.GET, "/test",
+            /* 
+             * El error de la instancias de la clase aninada
+             * se arregla cuando la clase anidada se define como
+             * estatica, aun no entiendo muy bien por que
+             */
             (request) -> new HttpResponse.Builder()
                         .setStatusCode(200)
                         .addHeader("Content-Type", "text/html")
                         .setEntity("<HTML> <P> Hello There... </P> </HTML>")
                         .build());
-
-
+    
             server.start();
         } catch (Exception e) {
            System.out.println("Hubo un error en el servidor: " + e.getMessage());
         }
     }
+    
+
+
 }
